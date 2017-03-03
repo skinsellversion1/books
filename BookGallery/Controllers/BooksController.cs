66433f640bbfde54e5e199d3ba8f666773d256ba
kinses22 <stephenkinsella20@hotmail.com>
2017-03-03 21:07:27 +0000

    Added controllers and method

---
+++ BookGallery/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGallery.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public string Detail() { return "Hello world"; }
+
+
+    }
+}
