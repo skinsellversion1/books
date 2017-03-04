6432a4d42ab9ed2676832f1925af92030ebed692
kinses22 <stephenkinsella20@hotmail.com>
2017-03-04 14:21:14 +0000

    Viewbag used to pass values from controller to view

--- BookGallery/Controllers/BooksController.cs
+++ BookGallery/Controllers/BooksController.cs
@@ -8,9 +8,21 @@ namespace BookGallery.Controllers
 {
     public class BooksController : Controller
     {
+
+
         // GET: Books
         public ActionResult Detail()
         {
+            ViewBag.title = "1984";
+            ViewBag.author = "G Orwell";
+            ViewBag.description = "An overview of how a society can be run";
+            ViewBag.awards = new String[]
+                {
+                "Best book 2016",
+                "Best book 1999",
+                "Best book 1996"
+                };
+
             return View();
         }
     }
