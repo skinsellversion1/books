cfa55738477803a3a13bc4f4e5925190a10f5915
kinses22 <stephenkinsella20@hotmail.com>
2017-03-06 11:26:22 +0000

    Updated Model

---
+++ BookGallery/Models/Awards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookGallery.Models
+{
+    public class Awards
+    {
+
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ BookGallery/Models/book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookGallery.Models
+{
+    public class book
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Awards[] Awards { get; set; }
+        public bool Fav { get; set; }
+
+
+        public string DisplayText
+        {
+            get
+            {
+                return Title + " by " + Author;
+            }
+        }
+
+
+        public string CoverImage
+        {
+            get
+            {
+                return Title.Replace(" ", "-").ToLower() + "-" + Author + ".jpg";
+            }
+
+        }
+
+
+    }
+}
